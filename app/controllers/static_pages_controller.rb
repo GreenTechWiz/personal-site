23fb5ee704f31135278523c7caf590ef08edea30
GreenTechWiz <aggreen0405@gmail.com>
2019-03-06 19:11:19 -0600

    My personal website app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  
+  def about
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  get "/" => 'static_pages#about'
+  get "/about" => 'static_pages#about'
+  get "/portfolio" => 'static_pages#portfolio'
+  get "/contact" => 'static_pages#contact'
+
+end
